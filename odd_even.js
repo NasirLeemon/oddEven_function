2ce29246eac49b15bb9bdf0930a78618828651f6
Leemon <leemon174@gmail.com>
2022-05-26 23:55:03 +0600

    first commit

---
+++ odd_even.js
@@ -0,0 +1,15 @@
+
+
+
+
+function oddEven(num) {
+    for (let i = 1; i <= num ; i++){
+        if ( i % 2 === 0){
+            console.log('Even');
+        } else {
+            console.log('Odd');
+        }
+    }
+}
+
+oddEven(10);
